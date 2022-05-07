5d39229bfe4d9618331e6c3507c730e94688d8ca
Collins <mutaic84@gmail.com>
2022-05-07 23:40:01 +0300

    add new pokemon data to database

--- src/components/PokemonForm.js
+++ src/components/PokemonForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Form } from "semantic-ui-react";
 
-function PokemonForm({ addNewPokemon }) {
+function PokemonForm({ addPokemon }) {
   const [formData, setFormData] = useState({})
 
   function handleDataInput(e) {
@@ -10,13 +10,31 @@ function PokemonForm({ addNewPokemon }) {
       [e.target.name]: e.target.value
     })
   }
+
+  function addNewPokemon() {
+    fetch(`http://localhost:3001/pokemon`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        name: formData.name,
+        hp: Number(formData.hp),
+        sprites: {
+          front: formData.frontUrl,
+          back: formData.backUrl
+        }
+      })
+    })
+    .then((res) => res.json())
+    .then((newData) => addPokemon(newData))
+  }
+
   return (
     <div>
       <h3>Add a Pokemon!</h3>
       <Form
-        onSubmit={() => {
-          addNewPokemon(formData);
-        }}
+        onSubmit={addNewPokemon}
       >
         <Form.Group widths="equal">
           <Form.Input fluid label="Name" placeholder="Name" name="name" onChange={handleDataInput}/>
