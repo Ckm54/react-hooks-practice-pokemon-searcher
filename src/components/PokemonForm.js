de3c81ac5439232406536086b2e76a037d1a2834
Collins <mutaic84@gmail.com>
2022-05-07 23:23:50 +0300

    capture form input fields

--- src/components/PokemonForm.js
+++ src/components/PokemonForm.js
@@ -1,29 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form } from "semantic-ui-react";
 
-function PokemonForm() {
+function PokemonForm({ addNewPokemon }) {
+  const [formData, setFormData] = useState({})
+
+  function handleDataInput(e) {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    })
+  }
   return (
     <div>
       <h3>Add a Pokemon!</h3>
       <Form
         onSubmit={() => {
-          console.log("submitting form...");
+          addNewPokemon(formData);
         }}
       >
         <Form.Group widths="equal">
-          <Form.Input fluid label="Name" placeholder="Name" name="name" />
-          <Form.Input fluid label="hp" placeholder="hp" name="hp" />
+          <Form.Input fluid label="Name" placeholder="Name" name="name" onChange={handleDataInput}/>
+          <Form.Input fluid label="hp" placeholder="hp" name="hp" onChange={handleDataInput}/>
           <Form.Input
             fluid
             label="Front Image URL"
             placeholder="url"
             name="frontUrl"
+            onChange={handleDataInput}
           />
           <Form.Input
             fluid
             label="Back Image URL"
             placeholder="url"
             name="backUrl"
+            onChange={handleDataInput}
           />
         </Form.Group>
         <Form.Button>Submit</Form.Button>
